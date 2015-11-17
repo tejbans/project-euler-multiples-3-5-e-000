66fd174088bd56dadd2058e3523d3f99ca778b32
tejbans <singh.tejbans@gmail.com>
2015-11-17 11:02:48 -0800

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  counter=1
+  list=[]
+  while counter<limit
+  if counter%3==0 || counter%5==0
+    list << counter
+    counter+=1
+    else
+      counter+=1
+    end
+  end
+  list
+end
+
+
+
+def sum_multiples(limit)
+    m = collect_multiples(limit)
+    t= m.reduce :+
+
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+class Multiples
+      
+   def initialize(limit)
+    @limit=limit
+    end
+
+    def collect_multiples
+      counter=1
+      list=[]
+     while counter<@limit
+      if counter%3==0 || counter%5==0
+        list<< counter
+        counter+=1
+        else
+        counter+=1
+      end
+    end
+    list
+    end
+
+    def sum_multiples
+      t= collect_multiples
+      sum= t.reduce :+
+    end
+
+
+end
